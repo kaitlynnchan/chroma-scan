a905cabb36cedb04189b4cffb69dc94e5dd50144
kaitlynnchan <kaitlynnchan@outlook.com>
2023-07-28 18:07:31 -0700

    Created colour evaluator and test image

--- src/main/java/com/chromascan/model/ColourEvaluator.java
+++ src/main/java/com/chromascan/model/ColourEvaluator.java
@@ -1,25 +1,60 @@
 package main.java.com.chromascan.model;
 
-public class ColourEvaluator {
-    
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-    // public ColourEvaluator(){
-    //     this.colour = new Colour();
+import javax.imageio.ImageIO;
+
+public class ColourEvaluator {
+
+    // private String file;
+    // private int x;
+    // private int y;
+
+    // public ColourEvaluator(String file, int x, int y){
+    //     this.file = file;
+    //     this.x = x;
+    //     this.y = y;
     // }
 
-    public static void main() {
+    public static void main(String[] args) {
+        String file = "resources/test-img2.png";
+        int x = 0;
+        int y = 0;
+
+        System.out.println("here1");
         ColourEvaluator ce = new ColourEvaluator();
 
-        String file = "";
-        int x = 0, y = 0;
-        
-        Source src = new Source(file);
-        Rgb rgbObj = src.getRgbFromPosition(x, y);
+        // Source src = new Source(file);
+        File srcImgFile;
+        BufferedImage image;
+        try {
+            srcImgFile = new File(file);
+            image = ImageIO.read(srcImgFile);
+        } catch (IOException e) {
+            System.out.println(e);
+            return;
+        }
+
+        // Get data for creating colour object
+        Rgb rgbObj = ce.getRgbFromPosition(image, x, y);
         String hex = ce.getHexFromRgb(rgbObj);
+        // create colour object
         Colour colour = new Colour(rgbObj, hex);
+
+        System.out.println(colour.getRgb().getRed() + ", " + colour.getRgb().getGreen() + ", " + colour.getRgb().getBlue());
+        System.out.println(colour.getHex());
     }
 
     private String getHexFromRgb(Rgb rgb){
-        return String.format("#%02x%02x%02x", rgb.getRed(), rgb.getGreen(), rgb.getBlue());
+        return String.format("#%02X%02X%02X", rgb.getRed(), rgb.getGreen(), rgb.getBlue());
+    }
+
+    private Rgb getRgbFromPosition(BufferedImage image, int x, int y){
+        int pixel = image.getRGB(x, y);
+        Color color = new Color(pixel, true);
+        return new Rgb(color.getRed(), color.getGreen(), color.getBlue());
     }
 }
